71c93d70cb8820da5e0216b0630a75a8e8764c7a
Toon <toonschaap3@gmail.com>
2019-01-11 10:22:30 +0100

    project

---
+++ New Unity Project/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+        Instantiate(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                EnemyRespawn();
+            }
+        }
+	
+    void EnemyRespawn()
+    {
+        Destroy(gameObject);
+        Instantiate(gameObject);
+        Debug.Log("Enemy removed");
+    }
+}
---
+++ New Unity Project/Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour {
+    public int score;
+    public Text scoretext;
+	// Use this for initialization
+	void Start () {
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+          score = score + 1;
+            ScoreUpdate();
+        }
+	}
+
+    void ScoreUpdate()
+    {
+        scoretext.text = "Enemies killed:" + score;
+    }
+}
